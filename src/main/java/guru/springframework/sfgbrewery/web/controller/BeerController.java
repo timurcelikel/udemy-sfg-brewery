3af86cc04062417fd127112880868c0c2d2cde06
Timur Celikel <timur.celikel@proton.me>
2023-10-08 22:32:35 -0700

    Added getBeer logging

--- src/main/java/guru/springframework/sfgbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/sfgbrewery/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgbrewery.web.controller;
 
 import guru.springframework.sfgbrewery.services.BeerService;
 import guru.springframework.sfgbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
 @RestController
+@Slf4j
 public class BeerController {
 
 	private final BeerService beerService;
@@ -22,7 +24,8 @@ public class BeerController {
 
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
-
+		final BeerDto beerDto = beerService.getBeerById(beerId);
+		log.info(String.valueOf(beerDto));
 		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
 	}
 
@@ -32,14 +35,15 @@ public class BeerController {
 		BeerDto savedDto = beerService.saveNewBeer(beerDto);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
-		return new ResponseEntity<HttpHeaders>(headers, HttpStatus.CREATED);
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{beerId}")
-	public ResponseEntity<HttpHeaders> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+	public ResponseEntity<HttpHeaders> handleUpdate(@PathVariable("beerId") UUID beerId,
+			@RequestBody BeerDto beerDto) {
 
 		beerService.updateBeer(beerId, beerDto);
-		return new ResponseEntity<HttpHeaders>(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 	@DeleteMapping("/{beerId}")
